0fa8b0928e957dae93275dbeeec57e4126f98f1d
rossrn <ronnross@Ronns-MacBook-Pro.local>
2014-12-01 13:30:23 -0500

    Isomorphic example now with server and client rendering

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 Isomorphic Example
 --
-Very barebones example of isomorphic rendering using React, Express, Webpack.
+Example of isomorphic rendering using React, Express, Webpack.
+
+Using Superagent to make both server-side and client-side request.
 
 steps to run
 --
 - npm install
 - webpack
 - node(mon) server.js
+
+App should be running at
+[localhost:3000](http://localhost:3000) 
--- elements/HelloWorld.jsx
+++ elements/HelloWorld.jsx
@@ -1,10 +1,28 @@
-var React = require('react');
+var React = require('react'),
+    request = require('superagent'),
+    utils = require('../utils/ServiceAPI');
 
 var HelloWorld = React.createClass({
   getInitialState: function() {
-    return {switched: false};
+    return {
+      switched: false,
+      caller: ''};
+  },
+  componentDidMount: function() {
+    var self = this;
+    utils.getCaller('/server', function(res){
+      self.setState({
+        caller: res.body
+      });
+    });
   },
   handleClick: function(event) {
+    var self = this;
+    utils.getCaller('/client', function(res){
+      self.setState({
+        caller: res.body
+      });
+    });
     this.setState({switched: !this.state.switched});
   },
   render: function() {
@@ -12,6 +30,7 @@ var HelloWorld = React.createClass({
     return (
       <div>
         Hello World Element <br />
+        Called from - {this.state.caller} <br />
         <button onClick={this.handleClick}>
           This switch is <b> {text} </b>. Click to toggle.
         </button>
