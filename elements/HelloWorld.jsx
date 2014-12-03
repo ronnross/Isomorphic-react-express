fbb7ba17734fda5a9b9bc53f3ac24d7552c0cc02
hartmamt <hartmamt@Matthews-MacBook-Pro-3.local>
2014-12-03 07:54:20 -0500

    added basic mcfly

--- elements/HelloWorld.jsx
+++ elements/HelloWorld.jsx
@@ -1,8 +1,11 @@
 var React = require('react'),
     request = require('superagent'),
-    utils = require('../utils/ServiceAPI');
+    utils = require('../utils/ServiceAPI'),
+    FluxCounterActions = require('../actions/FluxCounterActions'),
+    CounterStore = require('../stores/CounterStore');
 
 var HelloWorld = React.createClass({
+  mixins: [CounterStore.mixin],
   getInitialState: function() {
     return {
       switched: false,
@@ -12,12 +15,14 @@ var HelloWorld = React.createClass({
     var self = this;
     utils.getCaller('/server', function(res){
       self.setState({
-        caller: res.body
+        caller: res.body,
+        count: CounterStore.getCount()
       });
     });
   },
   handleClick: function(event) {
     var self = this;
+    FluxCounterActions.countOne();
     utils.getCaller('/client', function(res){
       self.setState({
         caller: res.body
@@ -25,12 +30,18 @@ var HelloWorld = React.createClass({
     });
     this.setState({switched: !this.state.switched});
   },
+  onChange: function() {
+    this.setState({
+      count: CounterStore.getCount()
+    });
+  },
   render: function() {
     var text = this.state.switched ? 'on' : 'off';
     return (
       <div>
         Hello World Element <br />
-        Called from - {this.state.caller} <br />
+        <p>{this.state.count}</p>
+        Called a from - {this.state.caller} <br />
         <button onClick={this.handleClick}>
           This switch is <b> {text} </b>. Click to toggle.
         </button>
